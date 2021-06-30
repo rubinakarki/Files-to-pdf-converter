64052d2c7e68e49d48184a6d1e23611a5d36d7b9
rubina123 <rubinakarki987@gmail.com>
2021-06-30 11:23:56 +0545

    [x] requirements.txt updated

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 import subprocess
 import requests
 
-url = 'https://s3-us-west-2.amazonaws.com/fuse-ai-resources-dev/onsiteinstructorassignments/5fd6f95c67b0602166815462/5fd6f95d67b0602166815464Screenshot_from_2020-11-20_18-42-48.png'
-filename,file_exten = (url.split('/')[-1]).split('.')[0],(url.split('/')[-1]).split('.')[1]
+url = 'https://s3-us-west-2.amazonaws.com/fuse-ai-resources-dev/onsiteinstructorassignments/60da9d683aa49a40cd061c2d/60da9d683aa49a40cd061c2fscrum.docx'
+
+foldername,filename,file_exten = url.split('/')[5],(url.split('/')[-1]).split('.')[0],(url.split('/')[-1]).split('.')[1]
 r = requests.get(url) 
- 
-# import ipdb;ipdb.set_trace()
 
 # storing file locally
 with open(f"/home/rubina/uno-test/{filename}.{file_exten}", 'wb') as f:
@@ -15,9 +14,15 @@ with open(f"/home/rubina/uno-test/{filename}.{file_exten}", 'wb') as f:
 convert_command  = f"unoconv -f pdf {filename}.{file_exten}"
 subprocess.call(convert_command,shell=True)
 
-upload_to_s3_command = f"aws s3 cp {filename}.pdf s3://fuse-ai-resources-dev/new"
+upload_to_s3_command = f"aws s3 cp {filename}.pdf s3://fuse-ai-resources-dev/onsiteinstructorassignments/{foldername}"
 subprocess.call(upload_to_s3_command,shell=True)
+
+# boto3
 
 # remove file locally
 remove_command = f"rm {filename}.{file_exten} && rm {filename}.pdf"
 subprocess.call(remove_command,shell=True)
+
+output_url = f"https://s3-us-west-2.amazonaws.com/fuse-ai-resources-dev/onsiteinstructorassignments/{foldername}/converted/{filename}.pdf"
+print(output_url)
+
